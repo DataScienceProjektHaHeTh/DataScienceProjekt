00d069a2cfa169c2fa91c21320a04a9c0eeeebf4
Jan Ole Hansen <jan.hansen2004@gmx.de>
2026-03-03 12:31:58 +0100

    Improved Sentiment analyses and implementet basic articles cleaning

--- src/processing/sentiment.py
+++ src/processing/sentiment.py
@@ -2,13 +2,13 @@ import pandas as pd
 from transformers import pipeline
 
 #load the csv file into a pandas dataframe
-df = pd.read_csv('data/raw/articles.csv')
+df = pd.read_csv('data/processed/articles_cleaned.csv')
 
 #initialize the sentiment analysis pipeline
 sentiment = pipeline("sentiment-analysis")
 
 #apply the sentiment analysis pipeline to the 'content' column of the dataframe
-results = sentiment(df['body'].tolist(), truncation=True, max_length=512)
+results = sentiment(df['body'].tolist(), batch_size=32, truncation=True, max_length=256)
 
 #add the results to the dataframe
 df['sentiment'] = [result['label'] for result in results]
