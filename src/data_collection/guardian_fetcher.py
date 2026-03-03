f5ef8c623acf61d4628550f3dc74bb308718ec93
nicothlt <171329225+nicothlt@users.noreply.github.com>
2026-03-03 11:46:06 +0100

    Added total articles counter

--- src/data_collection/guardian_fetcher.py
+++ src/data_collection/guardian_fetcher.py
@@ -21,10 +21,14 @@ response = requests.get("https://content.guardianapis.com/search", params=params
 if response.status_code != 200:
     print(f"[FEHLER] HTTP {response.status_code}")
     print(response.text)
+
 else:
     articles = response.json()["response"]["results"]
     print(f"[OK] {len(articles)} Artikel erhalten")
 
+    total = response.json()["response"]["total"]
+    print(f"[OK] {len(articles)} von {total} Artikeln erhalten")
+    
     os.makedirs("data/raw", exist_ok=True)
 
     with open("data/raw/articles.csv", "w", newline="", encoding="utf-8") as f:
